5594df267cb62b52567d7e80011223015b76da6a
Ye Bhone Lin <106800189+Ye-Bhone-Lin@users.noreply.github.com>
2025-04-18 21:39:28 +0630

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,9 +1,9 @@
 import requests
 
-response_code = requests.post('http://localhost:8000/coding/invoke', json={"input":{"topic":"discord bot"}})
+response_code = requests.post('https://langchain-deployment-dwwo.onrender.com/coding/invoke', json={"input":{"topic":"discord bot"}})
 
-#print(response_code.json()['output']['content'])
+print(response_code.json()['output']['content'])
 
-response_research = requests.post("http://localhost:8000/research/invoke", json={"input":{"topic":"AI"}})
+response_research = requests.post("https://langchain-deployment-dwwo.onrender.com/research/invoke", json={"input":{"topic":"AI"}})
 
-#print(response_research.json()['output']['content'])
+print(response_research.json()['output']['content'])
